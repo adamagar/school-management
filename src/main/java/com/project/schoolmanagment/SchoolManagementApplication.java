7cbff4dc1cb59ba48e03867a19def145b941df45
Adam Agar <adamagar88@gmail.com>
2023-10-22 19:04:07 -0600

    simulation of code sharing

--- src/main/java/com/project/schoolmanagment/SchoolManagementApplication.java
+++ src/main/java/com/project/schoolmanagment/SchoolManagementApplication.java
@@ -11,7 +11,7 @@ public class SchoolManagementApplication{
         SpringApplication.run(SchoolManagementApplication.class, args);
         //****---------
         //*****-------
-        //*************
+        //************
 
     }
 
